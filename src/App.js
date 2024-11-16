d43cc6169c0b901837a47e99b55c8ec34fb94b06
OlegBon <oleg_bon@ukr.net>
2024-11-16 13:53:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const onAddTaskHandler = () => {
+    let newTask = {
+      id: new Date(),
+      title: taskTitle,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    setTaskTitle("");
+  };
+
+  const onChangeHandler = (e) => {
+    setTaskTitle(e.target.value);
+  };
+
+  const changeTaskStateHandler = (id) => {
+    let newTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(newTasks);
+  };
+
+  const removeAllTasksHandler = () => {
+    setTasks([]);
+  };
+
+  const removeTask = (id) => {
+    setTasks([...tasks.filter((task) => task.id !== id)]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo list</h1>
+      <div className="Navigation">
+        <input
+          type="text"
+          onChange={(e) => onChangeHandler(e)}
+          placeholder="Add task"
+          value={taskTitle}
+        />
+        <button onClick={onAddTaskHandler}>Add Task</button>
+        <button onClick={removeAllTasksHandler}>Remove All Task</button>
+      </div>
+      <div className="Tasks">
+        {tasks.map((task) => (
+          <div
+            className={task.completed ? "Task Completed" : "Task"}
+            key={task.id}
+          >
+            <input
+              type="checkbox"
+              defaultChecked={task.completed}
+              // onInput прибираємо тому що є onChange
+              // onInput={onChangeHandler}
+              onChange={() => changeTaskStateHandler(task.id)}
+            />
+            <h1>{task.title}</h1>
+            <button onClick={() => removeTask(task.id)}>Delete/Drop</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
